c3e54ecbdb10bb16cc91ab5fba71611b92ffee93
Yasin <yaseen.ahmad2009@gmail.com>
2022-10-28 09:46:00 +0530

    database

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
    const conn = await mongoose.connect(
-    "mongodb://localhost:27017/coaching",
+    "mongodb+srv://aarifsolankey:3rcOOTPWrm5j1sc1@cluster0.vpnjy0x.mongodb.net/?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
